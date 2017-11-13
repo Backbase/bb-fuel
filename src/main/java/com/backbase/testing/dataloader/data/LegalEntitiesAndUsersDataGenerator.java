09cff3aa7071824150de5072c501edca753e2ab9
Kwo Ding <kwo@backbase.com>
2017-11-13 11:04:58 +0100

    Fix legal entity name

--- src/main/java/com/backbase/testing/dataloader/data/LegalEntitiesAndUsersDataGenerator.java
+++ src/main/java/com/backbase/testing/dataloader/data/LegalEntitiesAndUsersDataGenerator.java
@@ -19,8 +19,7 @@ public class LegalEntitiesAndUsersDataGenerator {
     public LegalEntitiesPostRequestBody generateRootLegalEntitiesPostRequestBody(String externalLegalEntityId) {
         return new LegalEntitiesPostRequestBody()
                 .withExternalId(externalLegalEntityId)
-                .withName(String.format(faker.company()
-                        .name(), faker.lorem().characters(8)))
+                .withName(faker.company().name() + faker.lorem().characters(8))
                 .withParentExternalId(null)
                 .withType(LegalEntityType.BANK);
     }
@@ -28,8 +27,7 @@ public class LegalEntitiesAndUsersDataGenerator {
     public LegalEntitiesPostRequestBody generateLegalEntitiesPostRequestBody(String externalLegalEntityId, String externalParentLegalEntityId) {
         return new LegalEntitiesPostRequestBody()
                 .withExternalId(externalLegalEntityId)
-                .withName(String.format(faker.company()
-                        .name(), faker.lorem().characters(8)))
+                .withName(faker.company().name() + faker.lorem().characters(8))
                 .withParentExternalId(externalParentLegalEntityId)
                 .withType(LegalEntityType.CUSTOMER);
     }

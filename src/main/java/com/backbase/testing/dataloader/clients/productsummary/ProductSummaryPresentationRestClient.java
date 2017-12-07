8046362a8129d477718f2be9f1fcdacc99cf1cbf
Kwo Ding <kwo@backbase.com>
2017-12-07 09:45:44 +0100

    Implement usage of new context arrangements endpoint for initiating payments

--- src/main/java/com/backbase/testing/dataloader/clients/productsummary/ProductSummaryPresentationRestClient.java
+++ src/main/java/com/backbase/testing/dataloader/clients/productsummary/ProductSummaryPresentationRestClient.java
@@ -1,6 +1,7 @@
 package com.backbase.testing.dataloader.clients.productsummary;
 
 import com.backbase.testing.dataloader.clients.common.RestClient;
+import com.backbase.testing.dataloader.dto.ProductSummaryQueryParameters;
 import com.backbase.testing.dataloader.utils.GlobalProperties;
 import io.restassured.response.Response;
 
@@ -13,6 +14,7 @@ public class ProductSummaryPresentationRestClient extends RestClient {
     private static final String SERVICE_VERSION = "v2";
     private static final String ENDPOINT_PRODUCTSUMMARY_PRESENTATION_SERVICE = "/product-summary-presentation-service/" + SERVICE_VERSION + "/productsummary";
     private static final String ENDPOINT_ARRANGEMENTS = ENDPOINT_PRODUCTSUMMARY_PRESENTATION_SERVICE + "/arrangements";
+    private static final String ENDPOINT_CONTEXT_ARRANGEMENTS = ENDPOINT_PRODUCTSUMMARY_PRESENTATION_SERVICE + "/context/arrangements";
 
     public ProductSummaryPresentationRestClient() {
         super(globalProperties.getString(PROPERTY_INFRA_BASE_URI));
@@ -21,6 +23,23 @@ public class ProductSummaryPresentationRestClient extends RestClient {
 
     public Response getProductSummaryArrangements() {
         return requestSpec()
+                .get(ENDPOINT_ARRANGEMENTS);
+    }
+
+    public Response getProductSummaryContextArrangements(ProductSummaryQueryParameters queryParameters) {
+        return requestSpec()
+                .queryParam("businessFunction", queryParameters.getBusinessFunction())
+                .queryParam("resourceName", queryParameters.getResourceName())
+                .queryParam("privilege", queryParameters.getPrivilege())
+                .queryParam("externalTransferAllowed", queryParameters.getExternalTransferAllowed())
+                .queryParam("creditAccount", queryParameters.getCreditAccount())
+                .queryParam("debitAccount", queryParameters.getDebitAccount())
+                .queryParam("from", queryParameters.getFrom())
+                .queryParam("size", queryParameters.getSize())
+                .queryParam("orderBy", queryParameters.getOrderBy())
+                .queryParam("direction", queryParameters.getDirection())
+                .queryParam("productKindName", queryParameters.getProductKindName())
+                .queryParam("searchTerm", queryParameters.getSearchTerm())
                 .get(ENDPOINT_ARRANGEMENTS);
     }
 }
--- src/main/java/com/backbase/testing/dataloader/configurators/PaymentsConfigurator.java
+++ src/main/java/com/backbase/testing/dataloader/configurators/PaymentsConfigurator.java
@@ -1,21 +1,17 @@
 package com.backbase.testing.dataloader.configurators;
 
 import com.backbase.dbs.presentation.paymentorder.rest.spec.v2.paymentorders.InitiatePaymentOrder;
-import com.backbase.presentation.accessgroup.rest.spec.v2.accessgroups.users.ArrangementPrivilegesGetResponseBody;
-import com.backbase.presentation.productsummary.rest.spec.v2.productsummary.ProductSummaryByLegalEntityIdGetResponseBody;
-import com.backbase.presentation.user.rest.spec.v2.users.UserGetResponseBody;
-import com.backbase.testing.dataloader.clients.accessgroup.AccessGroupPresentationRestClient;
+import com.backbase.presentation.productsummary.rest.spec.v2.productsummary.ArrangementsByBusinessFunctionGetResponseBody;
 import com.backbase.testing.dataloader.clients.common.LoginRestClient;
 import com.backbase.testing.dataloader.clients.payment.PaymentOrderPresentationRestClient;
 import com.backbase.testing.dataloader.clients.productsummary.ProductSummaryPresentationRestClient;
-import com.backbase.testing.dataloader.clients.user.UserPresentationRestClient;
 import com.backbase.testing.dataloader.data.PaymentsDataGenerator;
+import com.backbase.testing.dataloader.dto.ProductSummaryQueryParameters;
 import com.backbase.testing.dataloader.utils.CommonHelpers;
 import com.backbase.testing.dataloader.utils.GlobalProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -23,9 +19,7 @@ import java.util.stream.Collectors;
 
 import static com.backbase.testing.dataloader.data.CommonConstants.PROPERTY_PAYMENTS_MAX;
 import static com.backbase.testing.dataloader.data.CommonConstants.PROPERTY_PAYMENTS_MIN;
-import static com.backbase.testing.dataloader.data.CommonConstants.USER_ADMIN;
 import static org.apache.http.HttpStatus.SC_ACCEPTED;
-import static org.apache.http.HttpStatus.SC_OK;
 
 public class PaymentsConfigurator {
 
@@ -34,21 +28,19 @@ public class PaymentsConfigurator {
 
     private Random random = new Random();
     private PaymentOrderPresentationRestClient paymentOrderPresentationRestClient = new PaymentOrderPresentationRestClient();
-    private PaymentsDataGenerator paymentsDataGenerator = new PaymentsDataGenerator();
     private LoginRestClient loginRestClient = new LoginRestClient();
-    private AccessGroupPresentationRestClient accessGroupPresentationRestClient = new AccessGroupPresentationRestClient();
-    private UserPresentationRestClient userPresentationRestClient = new UserPresentationRestClient();
     private ProductSummaryPresentationRestClient productSummaryPresentationRestClient = new ProductSummaryPresentationRestClient();
+    private PaymentsDataGenerator paymentsDataGenerator = new PaymentsDataGenerator();
 
     private static final String ENTITLEMENTS_PAYMENTS_FUNCTION_NAME = "SEPA CT";
     private static final String ENTITLEMENTS_PAYMENTS_RESOURCE_NAME = "Payments";
     private static final String PRIVILEGE_CREATE = "create";
 
     public void ingestPaymentOrders(String externalUserId) {
-        List<ProductSummaryByLegalEntityIdGetResponseBody> sepaArrangements = getSepaArrangementsByExternalUserId(externalUserId);
+        List<ArrangementsByBusinessFunctionGetResponseBody> sepaArrangements = getSepaArrangementsByExternalUserId(externalUserId);
 
         for (int i = 0; i < CommonHelpers.generateRandomNumberInRange(globalProperties.getInt(PROPERTY_PAYMENTS_MIN), globalProperties.getInt(PROPERTY_PAYMENTS_MAX)); i++) {
-            ProductSummaryByLegalEntityIdGetResponseBody randomArrangement = sepaArrangements.get(random.nextInt(sepaArrangements.size()));
+            ArrangementsByBusinessFunctionGetResponseBody randomArrangement = sepaArrangements.get(random.nextInt(sepaArrangements.size()));
             InitiatePaymentOrder initiatePaymentOrder = paymentsDataGenerator.generateInitiatePaymentOrder(randomArrangement.getId());
             paymentOrderPresentationRestClient.initiatePaymentOrder(initiatePaymentOrder)
                     .then()
@@ -58,43 +50,20 @@ public class PaymentsConfigurator {
         }
     }
 
-    private List<ProductSummaryByLegalEntityIdGetResponseBody> getSepaArrangementsByExternalUserId(String externalUserId) {
-        loginRestClient.login(USER_ADMIN, USER_ADMIN);
-        String internalUserId = userPresentationRestClient.getUserByExternalId(externalUserId)
-                .then()
-                .statusCode(SC_OK)
-                .extract()
-                .as(UserGetResponseBody.class)
-                .getId();
-
-        ArrangementPrivilegesGetResponseBody[] arrangementPrivilegesGetResponseBodies = accessGroupPresentationRestClient.getListOfArrangementsWithPrivilegesForUser(internalUserId, ENTITLEMENTS_PAYMENTS_FUNCTION_NAME, ENTITLEMENTS_PAYMENTS_RESOURCE_NAME, PRIVILEGE_CREATE)
-                .then()
-                .statusCode(SC_OK)
-                .extract()
-                .as(ArrangementPrivilegesGetResponseBody[].class);
-
+    public List<ArrangementsByBusinessFunctionGetResponseBody> getSepaArrangementsByExternalUserId(String externalUserId) {
         loginRestClient.login(externalUserId, externalUserId);
-        ProductSummaryByLegalEntityIdGetResponseBody[] ProductSummaryByLegalEntityIdGetResponseBodies = productSummaryPresentationRestClient.getProductSummaryArrangements()
+
+        ArrangementsByBusinessFunctionGetResponseBody[] arrangements = productSummaryPresentationRestClient.getProductSummaryContextArrangements(new ProductSummaryQueryParameters()
+                .withBusinessFunction(ENTITLEMENTS_PAYMENTS_FUNCTION_NAME)
+                .withResourceName(ENTITLEMENTS_PAYMENTS_RESOURCE_NAME)
+                .withPrivilege(PRIVILEGE_CREATE)
+                .withSize(999))
                 .then()
-                .statusCode(SC_OK)
                 .extract()
-                .as(ProductSummaryByLegalEntityIdGetResponseBody[].class);
+                .as(ArrangementsByBusinessFunctionGetResponseBody[].class);
 
-        List<ProductSummaryByLegalEntityIdGetResponseBody> paymentsArrangements = new ArrayList<>();
-
-        for (ArrangementPrivilegesGetResponseBody arrangementPrivilegesGetResponseBody : arrangementPrivilegesGetResponseBodies) {
-            ProductSummaryByLegalEntityIdGetResponseBody productSummaryByLegalEntityIdGetResponseBody = Arrays.stream(ProductSummaryByLegalEntityIdGetResponseBodies)
-                    .filter(ps -> ps.getId()
-                            .equals(arrangementPrivilegesGetResponseBody.getArrangementId()))
-                    .findFirst()
-                    .orElse(null);
-
-            if (productSummaryByLegalEntityIdGetResponseBody != null) {
-                paymentsArrangements.add(productSummaryByLegalEntityIdGetResponseBody);
-            }
-        }
         // Make sure the Regex is in sync with payment-order-presentation-service/src/main/resources/application.yml (property: sepacountries)
-        return paymentsArrangements.stream()
+        return Arrays.stream(arrangements)
                 .filter(arrangement -> arrangement.getIBAN()
                         .matches("^(AT|BE|BG|CH|CY|CZ|DE|DK|EE|ES|FI|FR|GB|GI|GR|HR|HU|IE|IS|IT|LI|LT|LU|LV|MC|MT|NL|NO|PL|PT|RO|SE|SI|SK|SM)[a-zA-Z0-9_.-]*"))
                 .collect(Collectors.toList());

98b500f7a1ef84af1c2bb3c24ce63604683801d8
Anil Kusaksiz <anilk@backbase.com>
2018-01-22 14:53:13 +0100

    Added US Domestic Wire payments.

--- src/main/java/com/backbase/testing/dataloader/data/PaymentsDataGenerator.java
+++ src/main/java/com/backbase/testing/dataloader/data/PaymentsDataGenerator.java
@@ -1,6 +1,7 @@
 package com.backbase.testing.dataloader.data;
 
 import com.backbase.dbs.presentation.paymentorder.rest.spec.v2.paymentorders.AccountIdentification;
+import com.backbase.dbs.presentation.paymentorder.rest.spec.v2.paymentorders.Bank;
 import com.backbase.dbs.presentation.paymentorder.rest.spec.v2.paymentorders.Identification;
 import com.backbase.dbs.presentation.paymentorder.rest.spec.v2.paymentorders.IdentifiedPaymentOrder;
 import com.backbase.dbs.presentation.paymentorder.rest.spec.v2.paymentorders.InitiateCreditTransaction;
@@ -12,6 +13,7 @@ import com.backbase.rest.spec.common.types.Currency;
 import com.backbase.testing.dataloader.utils.CommonHelpers;
 import com.github.javafaker.Faker;
 import org.apache.commons.lang.time.DateUtils;
+import org.bouncycastle.asn1.x509.qualified.Iso4217CurrencyCode;
 
 import java.text.SimpleDateFormat;
 import java.util.Collections;
@@ -26,7 +28,12 @@ public class PaymentsDataGenerator {
 
     public InitiatePaymentOrder generateInitiatePaymentOrder(String debtorArrangementId) {
         IdentifiedPaymentOrder.PaymentMode paymentMode = IdentifiedPaymentOrder.PaymentMode.values()[random.nextInt(IdentifiedPaymentOrder.PaymentMode.values().length)];
+        IdentifiedPaymentOrder.PaymentType paymentType = IdentifiedPaymentOrder.PaymentType.values()[random.nextInt(IdentifiedPaymentOrder.PaymentType.values().length)];
         Schedule schedule = null;
+        Bank creditorBank = null;
+        Bank correspondentBank = null;
+        Currency currency;
+        Identification identification;
 
         if (paymentMode == IdentifiedPaymentOrder.PaymentMode.RECURRING) {
             schedule = new Schedule()
@@ -38,9 +45,47 @@ public class PaymentsDataGenerator {
                     .withEndDate(new SimpleDateFormat("yyyy-MM-dd").format(DateUtils.addYears(new Date(), 1)));
         }
 
+        if (paymentType == IdentifiedPaymentOrder.PaymentType.US_DOMESTIC_WIRE) {
+            creditorBank = new Bank().withBankBranchCode("322172425")
+                    //TODO Hardcoded bank branch code as a temporary solution
+                    .withName(faker.name()
+                            .fullName())
+                    .withPostalAddress(new PostalAddress()
+                            .withAddressLine1(faker.address().streetAddress())
+                            .withAddressLine2(faker.address().secondaryAddress())
+                            .withStreetName(faker.address().streetAddress())
+                            .withPostCode(faker.address().zipCode())
+                            .withTown(faker.address().city())
+                            .withCountry(faker.address().countryCode())
+                            .withCountrySubDivision(faker.address().state()));
+            correspondentBank = new Bank().withBankBranchCode("044084354")
+                    //TODO Hardcoded bank branch code as a temporary solution
+                    .withName(faker.name()
+                            .fullName());
+            currency = new Currency()
+                    .withCurrencyCode("USD")
+                    .withAmount(CommonHelpers.generateRandomAmountInRange(1000L, 99999L));
+
+            identification = new Identification()
+                    .withSchemeName(Identification.SchemeName.BBAN)
+                    .withIdentification("4902331374");
+            //TODO Hardcoded BBAN as a temporary solution
+        }
+        else {
+            currency = new Currency()
+                    .withCurrencyCode("EUR")
+                    .withAmount(CommonHelpers.generateRandomAmountInRange(1000L, 99999L));
+
+            identification = new Identification()
+                    .withSchemeName(Identification.SchemeName.IBAN)
+                    .withIdentification(productSummaryDataGenerator.generateRandomIban());
+        }
+
         return new InitiatePaymentOrder()
                 .withDebtorAccount(new AccountIdentification()
-                        .withName(faker.lorem().sentence(3, 0).replace(".", ""))
+                        .withName(faker.lorem()
+                                .sentence(3, 0)
+                                .replace(".", ""))
                         .withIdentification(new Identification()
                                 .withSchemeName(Identification.SchemeName.ID)
                                 .withIdentification(debtorArrangementId)))
@@ -50,24 +95,25 @@ public class PaymentsDataGenerator {
                 .withRequestedExecutionDate(new SimpleDateFormat("yyyy-MM-dd").format(new Date().getTime()))
                 .withSchedule(schedule)
                 .withCreditTransferTransactionInformation(Collections.singletonList(new InitiateCreditTransaction()
-                        .withEndToEndIdentification(faker.lorem().characters(10))
+                        .withEndToEndIdentification(faker.lorem()
+                                .characters(10))
                         .withCreditorAccount(new AccountIdentification()
-                                .withIdentification(new Identification()
-                                        .withSchemeName(Identification.SchemeName.IBAN)
-                                        .withIdentification(productSummaryDataGenerator.generateRandomIban())))
-                        .withInstructedAmount(new Currency()
-                            .withCurrencyCode("EUR")
-                            .withAmount(CommonHelpers.generateRandomAmountInRange(1000L, 99999L)))
-                        .withRemittanceInformation(faker.lorem().sentence(3, 0).replace(".", ""))
+                                .withIdentification(identification))
+                        .withInstructedAmount(currency)
+                        .withRemittanceInformation(faker.lorem()
+                                .sentence(3, 0)
+                                .replace(".", ""))
                         .withCreditor(new InvolvedParty()
-                            .withName(faker.name().fullName())
-                            .withPostalAddress(new PostalAddress()
-                            .withAddressLine1(faker.address().streetAddress())
-                            .withAddressLine2(faker.address().secondaryAddress())
-                            .withStreetName(faker.address().streetAddress())
-                            .withPostCode(faker.address().zipCode())
-                            .withTown(faker.address().city())
-                            .withCountry(faker.address().countryCode())
-                            .withCountrySubDivision(faker.address().state())))));
+                                .withName(faker.name().fullName())
+                                .withPostalAddress(new PostalAddress()
+                                        .withAddressLine1(faker.address().streetAddress())
+                                        .withAddressLine2(faker.address().secondaryAddress())
+                                        .withStreetName(faker.address().streetAddress())
+                                        .withPostCode(faker.address().zipCode())
+                                        .withTown(faker.address().city())
+                                        .withCountry(faker.address().countryCode())
+                                        .withCountrySubDivision(faker.address().state())))
+                        .withCreditorBank(creditorBank)
+                        .withCorrespondentBank(correspondentBank)));
     }
 }

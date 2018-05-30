6b60275d6bd6270ff4333714eb821e8cb89e9fb4
Kwo Ding <kwo@ding.it>
2018-05-30 11:08:22 +0200

    Use synchronized list for parallel stream in TransactionsConfigurator

--- src/main/java/com/backbase/ct/dataloader/configurators/TransactionsConfigurator.java
+++ src/main/java/com/backbase/ct/dataloader/configurators/TransactionsConfigurator.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -35,7 +36,7 @@ public class TransactionsConfigurator {
     private Random random = new Random();
 
     public void ingestTransactionsByArrangement(String externalArrangementId) {
-        List<TransactionsPostRequestBody> transactions = new ArrayList<>();
+        List<TransactionsPostRequestBody> transactions = Collections.synchronizedList(new ArrayList<>());
         List<String> categoryNames = new ArrayList<>();
 
         if (globalProperties.getBoolean(PROPERTY_USE_PFM_CATEGORIES_FOR_TRANSACTIONS)) {

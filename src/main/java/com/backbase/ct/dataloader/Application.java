40c32814626821c692d3035b229dcff085ba930b
Richard Blank <richardb@backbase.com>
2018-06-26 16:35:42 +0200

    fix secret key as env var

--- src/main/java/com/backbase/ct/dataloader/Application.java
+++ src/main/java/com/backbase/ct/dataloader/Application.java
@@ -1,18 +1,21 @@
 package com.backbase.ct.dataloader;
 
 import com.backbase.buildingblocks.jwt.internal.config.EnableInternalJwtConsumer;
-import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableInternalJwtConsumer
-@ComponentScan(basePackages = {"com.backbase.buildingblocks.backend",
-    "com.backbase.ct.dataloader", "com.backbase.presentation", "com.backbase.integration"})
+@ComponentScan(basePackages = {
+    "com.backbase.ct.dataloader",
+    "com.backbase.buildingblocks.backend",
+    "com.backbase.presentation",
+    "com.backbase.integration"})
 public class Application {
 
     public static void main(String[] args) {
+        System.setProperty("SIG_SECRET_KEY", "JWTSecretKeyDontUseInProduction!");
         SpringApplication application = new SpringApplication(Application.class);
         application.setWebEnvironment(false);
         application.run(args);

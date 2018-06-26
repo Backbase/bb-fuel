0acd0ab1609bb5f439a0c5991e90143699a9caf5
Richard Blank <richardb@backbase.com>
2018-06-26 11:46:35 +0200

    Spring boot with commandline runner (without all dependy injection)

--- src/main/java/com/backbase/ct/dataloader/Application.java
+++ src/main/java/com/backbase/ct/dataloader/Application.java
@@ -1,21 +1,21 @@
 package com.backbase.ct.dataloader;
 
-import com.backbase.buildingblocks.backend.configuration.autoconfigure.BackbaseApplication;
-import java.io.IOException;
+import com.backbase.buildingblocks.jwt.internal.config.EnableInternalJwtConsumer;
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@BackbaseApplication
+@SpringBootApplication
+@EnableInternalJwtConsumer
+@ComponentScan(basePackages = {"com.backbase.buildingblocks.backend",
+    "com.backbase.ct.dataloader", "com.backbase.presentation", "com.backbase.integration"})
 public class Application {
 
     public static void main(String[] args) {
-
-        SpringApplication application = new SpringApplication(Runner.class);
+        SpringApplication application = new SpringApplication(Application.class);
         application.setWebEnvironment(false);
+        application.setBannerMode(Mode.OFF);
         application.run(args);
-        try {
-            Runner.doit(args);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
--- src/main/java/com/backbase/ct/dataloader/Runner.java
+++ src/main/java/com/backbase/ct/dataloader/Runner.java
@@ -9,44 +9,75 @@ import com.backbase.ct.dataloader.setup.ServiceAgreementsSetup;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
-public class Runner {
+@Component
+@RequiredArgsConstructor
+public class Runner implements ApplicationRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Runner.class);
 
-    public static void doit(String[] args) throws IOException {
-        final Logger LOGGER = LoggerFactory.getLogger(Runner.class);
+    private final AccessControlSetup accessControlSetup;
+    private final ServiceAgreementsSetup serviceAgreementsSetup;
+    private final CapabilitiesDataSetup capabilitiesDataSetup;
+    private final AccessControlHealthCheck accessControlHealthCheck;
+    private final ProductSummaryHealthCheck productSummaryHealthCheck;
+    private final TransactionsHealthCheck transactionsHealthCheck;
 
-        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "8");
+    @Override
+    public void run(ApplicationArguments args) {
+        try {
+            doIt();
+            System.exit(0);
+        } catch (IOException e) {
+            LOGGER.error("Failed setting up access", e);
+            System.exit(1);
+        }
+    }
 
-        AccessControlSetup accessControlSetup = new AccessControlSetup();
-        ServiceAgreementsSetup serviceAgreementsSetup = new ServiceAgreementsSetup();
-        CapabilitiesDataSetup capabilitiesDataSetup = new CapabilitiesDataSetup();
-        AccessControlHealthCheck accessControlHealthCheck = new AccessControlHealthCheck();
-        ProductSummaryHealthCheck productSummaryHealthCheck = new ProductSummaryHealthCheck();
-        TransactionsHealthCheck transactionsHealthCheck = new TransactionsHealthCheck();
-
-        accessControlHealthCheck.checkAccessControlServicesHealth();
-        productSummaryHealthCheck.checkProductSummaryServicesHealth();
-        transactionsHealthCheck.checkTransactionsServicesHealth();
+    /**
+     * Sponsored runner.
+     *
+     * @throws IOException when setupAccess throws it
+     */
+    private void doIt() throws IOException {
+        performHealthChecks();
 
         Instant start = Instant.now();
 
+        setupAccess();
+        ingestData();
+
+        logDuration(start);
+    }
+
+    private void performHealthChecks() {
+        accessControlHealthCheck.checkAccessControlServicesHealth();
+        productSummaryHealthCheck.checkProductSummaryServicesHealth();
+        transactionsHealthCheck.checkTransactionsServicesHealth();
+    }
+
+    private void setupAccess() throws IOException {
         accessControlSetup.setupBankWithEntitlementsAdminAndProducts();
         accessControlSetup.setupAccessControlForUsers();
         serviceAgreementsSetup.setupCustomServiceAgreements();
+    }
 
+    private void ingestData() {
         capabilitiesDataSetup.ingestBankNotifications();
         capabilitiesDataSetup.ingestContactsPerUser();
         capabilitiesDataSetup.ingestPaymentsPerUser();
         capabilitiesDataSetup.ingestConversationsPerUser();
         capabilitiesDataSetup.ingestActionsPerUser();
-
-        Instant end = Instant.now();
-        Duration duration = Duration.between(start, end);
-        long minutes = duration.getSeconds() / 60;
-        long seconds = duration.getSeconds() % 60;
-        LOGGER.info("Time to ingest data was " + minutes + " minutes and " + seconds + " seconds");
     }
 
+    private void logDuration(Instant start) {
+        Instant end = Instant.now();
+        long totalSeconds = Duration.between(start, end).getSeconds();
+        LOGGER.info("Time to ingest data was {} minutes and {} seconds", totalSeconds / 60, totalSeconds % 60);
+    }
 }

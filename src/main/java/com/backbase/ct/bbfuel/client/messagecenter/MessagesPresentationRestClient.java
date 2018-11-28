dfa126370e7d94c17d19d7e06088947967eef695
Oleksandr Iusko <oleksandri@backbase.com>
2018-11-28 13:17:16 +0200

    fix message-center setup due to changes in Manage Topics functionality: system topics have been removed; business function 'Manage Topics'has been added;

--- src/main/java/com/backbase/ct/bbfuel/client/messagecenter/MessagesPresentationRestClient.java
+++ src/main/java/com/backbase/ct/bbfuel/client/messagecenter/MessagesPresentationRestClient.java
@@ -3,6 +3,7 @@ package com.backbase.ct.bbfuel.client.messagecenter;
 import com.backbase.ct.bbfuel.client.common.AbstractRestClient;
 import com.backbase.dbs.messages.presentation.rest.spec.v4.messagecenter.ConversationDraftsPostRequestBody;
 import com.backbase.dbs.messages.presentation.rest.spec.v4.messagecenter.DraftsPostRequestBody;
+import com.backbase.dbs.messages.presentation.rest.spec.v4.messagecenter.TopicsPostRequestBody;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.springframework.stereotype.Component;
@@ -17,6 +18,7 @@ public class MessagesPresentationRestClient extends AbstractRestClient {
     private static final String ENDPOINT_SEND_DRAFT_REQUEST = ENDPOINT_DRAFTS + "/%s/send-draft-request";
     private static final String ENDPOINT_CONVERSATIONS = ENDPOINT_MESSAGE_CENTER + "/conversations";
     private static final String ENDPOINT_CONVERSATION_DRAFTS = ENDPOINT_CONVERSATIONS + "/%s/drafts";
+    private static final String ENDPOINT_TOPICS = ENDPOINT_MESSAGE_CENTER + "/topics";
 
     public MessagesPresentationRestClient() {
         super(SERVICE_VERSION);
@@ -25,29 +27,36 @@ public class MessagesPresentationRestClient extends AbstractRestClient {
 
     public Response postDraft(DraftsPostRequestBody body) {
         return requestSpec()
-            .contentType(ContentType.JSON)
-            .body(body)
-            .post(getPath(ENDPOINT_DRAFTS));
+                .contentType(ContentType.JSON)
+                .body(body)
+                .post(getPath(ENDPOINT_DRAFTS));
     }
 
     public Response sendDraftRequest(String draftId) {
         return requestSpec()
-            .contentType(ContentType.JSON)
-            .body("{}")
-            .post(String.format(getPath(ENDPOINT_SEND_DRAFT_REQUEST), draftId));
+                .contentType(ContentType.JSON)
+                .body("{}")
+                .post(String.format(getPath(ENDPOINT_SEND_DRAFT_REQUEST), draftId));
     }
 
     public Response postConversationDraft(ConversationDraftsPostRequestBody draft, String conversationId) {
         return requestSpec()
-            .contentType(ContentType.JSON)
-            .body(draft)
-            .post(String.format(getPath(ENDPOINT_CONVERSATION_DRAFTS), conversationId));
+                .contentType(ContentType.JSON)
+                .body(draft)
+                .post(String.format(getPath(ENDPOINT_CONVERSATION_DRAFTS), conversationId));
     }
 
     public Response getConversations() {
         return requestSpec()
-            .contentType(ContentType.JSON)
-            .get(getPath(ENDPOINT_CONVERSATIONS));
+                .contentType(ContentType.JSON)
+                .get(getPath(ENDPOINT_CONVERSATIONS));
+    }
+
+    public Response postTopic(TopicsPostRequestBody topicsPostRequestBody) {
+        return requestSpec()
+                .contentType(ContentType.JSON)
+                .body(topicsPostRequestBody)
+                .post(getPath(ENDPOINT_TOPICS));
     }
 
     @Override

c3feee47a1abb7894f88516b5088f3fb27a360a2
Daryna Grytsaieva <daryna@backbase.com>
2018-12-04 10:48:12 +0100

    Use new endpoint for user permissions

--- src/main/java/com/backbase/ct/bbfuel/client/accessgroup/AccessGroupIntegrationRestClient.java
+++ src/main/java/com/backbase/ct/bbfuel/client/accessgroup/AccessGroupIntegrationRestClient.java
@@ -1,16 +1,20 @@
 package com.backbase.ct.bbfuel.client.accessgroup;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 import static org.apache.http.HttpStatus.SC_OK;
 
 import com.backbase.ct.bbfuel.client.common.AbstractRestClient;
 import com.backbase.ct.bbfuel.data.CommonConstants;
+import com.backbase.integration.accessgroup.rest.spec.v2.accessgroups.IntegrationIdentifier;
 import com.backbase.integration.accessgroup.rest.spec.v2.accessgroups.config.functions.FunctionsGetResponseBody;
 import com.backbase.integration.accessgroup.rest.spec.v2.accessgroups.datagroups.DataGroupPostRequestBody;
 import com.backbase.integration.accessgroup.rest.spec.v2.accessgroups.functiongroups.FunctionGroupPostRequestBody;
-import com.backbase.integration.accessgroup.rest.spec.v2.accessgroups.users.permissions.AssignPermissionsPostRequestBody;
+import com.backbase.integration.accessgroup.rest.spec.v2.accessgroups.users.permissions.IntegrationAssignUserPermissions;
+import com.backbase.integration.accessgroup.rest.spec.v2.accessgroups.users.permissions.IntegrationFunctionGroupDataGroup;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
@@ -29,7 +33,8 @@ public class AccessGroupIntegrationRestClient extends AbstractRestClient {
     private static final String ENDPOINT_ACCESS_GROUPS = "/accessgroups";
     private static final String ENDPOINT_CONFIG_FUNCTIONS = ENDPOINT_ACCESS_GROUPS + "/config/functions";
     private static final String ENDPOINT_FUNCTION = ENDPOINT_ACCESS_GROUPS + "/function-groups";
-    private static final String ENDPOINT_USERS_PERMISSIONS = ENDPOINT_ACCESS_GROUPS + "/users/permissions";
+    private static final String ENDPOINT_USERS_PERMISSIONS =
+        ENDPOINT_ACCESS_GROUPS + "/users/permissions/user-permissions";
     private static final String ENDPOINT_DATA = ENDPOINT_ACCESS_GROUPS + "/data-groups";
 
     public AccessGroupIntegrationRestClient() {
@@ -78,24 +83,33 @@ public class AccessGroupIntegrationRestClient extends AbstractRestClient {
             .collect(Collectors.toList());
     }
 
-    public Response assignPermissions(AssignPermissionsPostRequestBody body) {
+    public Response assignPermissions(IntegrationAssignUserPermissions body) {
         return requestSpec()
             .contentType(ContentType.JSON)
             .body(body)
-            .post(getPath(ENDPOINT_USERS_PERMISSIONS));
+            .put(getPath(ENDPOINT_USERS_PERMISSIONS));
     }
 
     public Response assignPermissions(
         String externalUserId,
-        String internalServiceAgreementId,
+        String externalServiceAgreementId,
         String functionGroupId,
         List<String> dataGroupIds) {
-        return assignPermissions(new AssignPermissionsPostRequestBody()
-            .withExternalLegalEntityId(null)
+
+        IntegrationIdentifier functionGroupIdentifier = new IntegrationIdentifier()
+            .withIdIdentifier(functionGroupId);
+
+        List<IntegrationIdentifier> dataGroupIdentifiers = new ArrayList<>();
+        dataGroupIds.forEach(dataGroupId -> dataGroupIdentifiers.add(new IntegrationIdentifier()
+            .withIdIdentifier(dataGroupId)));
+
+
+        return assignPermissions(new IntegrationAssignUserPermissions()
             .withExternalUserId(externalUserId)
-            .withServiceAgreementId(internalServiceAgreementId)
-            .withFunctionGroupId(functionGroupId)
-            .withDataGroupIds(dataGroupIds));
+            .withExternalServiceAgreementId(externalServiceAgreementId)
+            .withFunctionGroupDataGroups(singletonList(new IntegrationFunctionGroupDataGroup()
+                .withFunctionGroupIdentifier(functionGroupIdentifier)
+                .withDataGroupIdentifiers(dataGroupIdentifiers))));
     }
 
     @Override
--- src/main/java/com/backbase/ct/bbfuel/client/user/UserIntegrationRestClient.java
+++ src/main/java/com/backbase/ct/bbfuel/client/user/UserIntegrationRestClient.java
@@ -4,9 +4,9 @@ import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.apache.http.HttpStatus.SC_CREATED;
 import static org.apache.http.HttpStatus.SC_OK;
 
+import com.backbase.buildingblocks.presentation.errors.BadRequestException;
 import com.backbase.ct.bbfuel.client.common.AbstractRestClient;
 import com.backbase.ct.bbfuel.data.CommonConstants;
-import com.backbase.integration.user.rest.spec.v2.users.BadRequestException;
 import com.backbase.integration.user.rest.spec.v2.users.EntitlementsAdminPostRequestBody;
 import com.backbase.integration.user.rest.spec.v2.users.UsersPostRequestBody;
 import io.restassured.http.ContentType;
@@ -39,8 +39,8 @@ public class UserIntegrationRestClient extends AbstractRestClient {
             && response.then()
                 .extract()
                 .as(BadRequestException.class)
-            .getErrorCode()
-            .equals("user.access.fetch.data.error.message.USER_ALREADY_ENTITLEMENTS_ADMIN")) {
+            .getMessage()
+            .equals("User is already entitlements admin")) {
             LOGGER.warn("Entitlements admin [{}] already exists under legal entity [{}], skipped ingesting this entitlements admin",
                     externalUserId, externalLegalEntityId);
         } else if (response.statusCode() == SC_OK) {

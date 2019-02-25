346d50f3ee30abce2f312922e4553651339f5598
Sian Webster <sian@backbase.com>
2019-02-25 09:35:38 +0000

    OBPAY-1352: Update with master changes

--- src/main/java/com/backbase/ct/bbfuel/client/billpay/BillPayPresentationRestClient.java
+++ src/main/java/com/backbase/ct/bbfuel/client/billpay/BillPayPresentationRestClient.java
@@ -1,32 +1,36 @@
 package com.backbase.ct.bbfuel.client.billpay;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Component;
 
-import com.backbase.ct.bbfuel.client.common.AbstractRestClient;
+import com.backbase.ct.bbfuel.client.common.RestClient;
+import com.backbase.ct.bbfuel.config.BbFuelConfiguration;
 
 import io.restassured.response.Response;
+import lombok.RequiredArgsConstructor;
 
 @Component
-public class BillPayPresentationRestClient extends AbstractRestClient {
+@RequiredArgsConstructor
+public class BillPayPresentationRestClient extends RestClient {
+    
+    private final BbFuelConfiguration config;
     
     private static final String SERVICE_VERSION = "v2";
     private static final String BILLPAY_PRESENTATION_SERVICE = "billpay-presentation-service";
     private static final String ENDPOINT_BILLPAY = "/bill-pay";
     private static final String ENDPOINT_ENROL = ENDPOINT_BILLPAY + "/enrolment";
     
-    public BillPayPresentationRestClient() {
-        super(SERVICE_VERSION);
-        setInitialPath(composeInitialPath());
+    @PostConstruct
+    public void init() {
+        setBaseUri(config.getPlatform().getGateway());
+        setVersion(SERVICE_VERSION);
+        setInitialPath(BILLPAY_PRESENTATION_SERVICE);
     }
     
     public Response enrolUser() {
         return requestSpec()
                         .post(getPath(ENDPOINT_ENROL));
-    }
-    
-    @Override
-    protected String composeInitialPath() {
-        return getGatewayURI() + SLASH + BILLPAY_PRESENTATION_SERVICE;
     }
 
 }

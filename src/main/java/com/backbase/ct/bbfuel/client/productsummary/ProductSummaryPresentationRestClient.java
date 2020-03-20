d3d1959936c16952c33e5c4d572539c520d048c8
Vinicius Silva <vinicius@backbase.com>
2020-03-20 11:05:53 +0100

    Product Summary Fixes - Fixing endpoints for ArrangementsIntegration & ProductSummary Presentation

--- src/main/java/com/backbase/ct/bbfuel/client/productsummary/ProductSummaryPresentationRestClient.java
+++ src/main/java/com/backbase/ct/bbfuel/client/productsummary/ProductSummaryPresentationRestClient.java
@@ -27,6 +27,7 @@ public class ProductSummaryPresentationRestClient extends RestClient {
     private final BbFuelConfiguration config;
 
     private static final String SERVICE_VERSION = "v2";
+    private static final String CLIENT_API = "/client_api";
     private static final String PRODUCT_SUMMARY_PRESENTATION_SERVICE = "product-summary-presentation-service";
     private static final String ENDPOINT_PRODUCT_SUMMARY = "/productsummary";
     private static final String ENDPOINT_ARRANGEMENTS = ENDPOINT_PRODUCT_SUMMARY + "/arrangements";
@@ -36,7 +37,7 @@ public class ProductSummaryPresentationRestClient extends RestClient {
     public void init() {
         setBaseUri(config.getPlatform().getGateway());
         setVersion(SERVICE_VERSION);
-        setInitialPath(PRODUCT_SUMMARY_PRESENTATION_SERVICE);
+        setInitialPath(PRODUCT_SUMMARY_PRESENTATION_SERVICE + CLIENT_API);
     }
 
     public Response getProductSummaryArrangements() {

246ed50852a3de96bd48726151132cede6e18362
Richard Blank <richardb@backbase.com>
2020-06-02 14:12:22 +0200

    CB-5043: remove drafts being deprecated API endpoint that was deleted in 2.18.0

--- src/main/java/com/backbase/ct/bbfuel/configurator/MessagesConfigurator.java
+++ src/main/java/com/backbase/ct/bbfuel/configurator/MessagesConfigurator.java
@@ -1,14 +1,8 @@
 package com.backbase.ct.bbfuel.configurator;
 
-import static com.backbase.ct.bbfuel.data.CommonConstants.PROPERTY_MESSAGES_MAX;
-import static com.backbase.ct.bbfuel.data.CommonConstants.PROPERTY_MESSAGES_MIN;
 import static com.backbase.ct.bbfuel.data.CommonConstants.PROPERTY_MESSAGE_TOPICS_MAX;
 import static com.backbase.ct.bbfuel.data.CommonConstants.PROPERTY_MESSAGE_TOPICS_MIN;
-import static com.backbase.ct.bbfuel.data.MessagesDataGenerator.generateConversationDraftsPostRequestBody;
-import static com.backbase.ct.bbfuel.data.MessagesDataGenerator.generateDraftsPostRequestBody;
-import static com.backbase.ct.bbfuel.util.CommonHelpers.getRandomFromList;
 import static java.util.Collections.singleton;
-import static org.apache.http.HttpStatus.SC_ACCEPTED;
 import static org.apache.http.HttpStatus.SC_OK;
 
 import com.backbase.ct.bbfuel.client.common.LoginRestClient;
@@ -18,11 +12,6 @@ import com.backbase.ct.bbfuel.data.MessagesDataGenerator;
 import com.backbase.ct.bbfuel.service.LegalEntityService;
 import com.backbase.ct.bbfuel.util.CommonHelpers;
 import com.backbase.ct.bbfuel.util.GlobalProperties;
-import com.backbase.dbs.messages.rest.spec.v4.messagecenter.DraftPostResponseBody;
-import com.backbase.dbs.messages.rest.spec.v4.messagecenter.MessageDraftsGetResponseBody;
-import com.backbase.dbs.messages.rest.spec.v4.messagecenter.MessageDraftsPostRequestBody;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -39,16 +28,13 @@ public class MessagesConfigurator {
     private final LegalEntityService legalEntityService;
     private final UserPresentationRestClient userPresentationRestClient;
 
-    public void ingestConversations(String externalUserId) {
-        int howManyMessages = CommonHelpers.generateRandomNumberInRange(globalProperties.getInt(PROPERTY_MESSAGES_MIN),
-                globalProperties.getInt(PROPERTY_MESSAGES_MAX));
+    public void ingestTopics() {
 
         int howManyTopics = CommonHelpers.generateRandomNumberInRange(
             globalProperties.getInt(PROPERTY_MESSAGE_TOPICS_MIN),globalProperties.getInt(PROPERTY_MESSAGE_TOPICS_MAX));
 
         loginRestClient.loginBankAdmin();
 
-        List<String> topicIds = new ArrayList<>();
         String bankAdmin = legalEntityService.getRootAdmin();
         String bankAdminInternalId = userPresentationRestClient.getUserByExternalId(bankAdmin).getId();
 
@@ -56,51 +42,11 @@ public class MessagesConfigurator {
             String topicId = messagesPresentationRestClient.postTopic(MessagesDataGenerator
                 .generateTopicPostRequestBody(singleton(bankAdminInternalId)))
                 .then()
-                .statusCode(SC_ACCEPTED)
-                .extract()
-                .path("id");
-            topicIds.add(topicId);
-
-            log.info("Topic ingested with id [{}] for subscriber [{}]", topicId, bankAdmin);
-        });
-
-        IntStream.range(0, howManyMessages).forEach(number -> {
-            loginRestClient.login(externalUserId, externalUserId);
-
-            MessageDraftsPostRequestBody draftsPostRequestBody = generateDraftsPostRequestBody(getRandomFromList(topicIds));
-            String draftId = messagesPresentationRestClient.postDraft(draftsPostRequestBody)
-                .then()
-                .statusCode(SC_ACCEPTED)
-                .extract()
-                .as(DraftPostResponseBody.class)
-                .getId();
-
-            messagesPresentationRestClient.sendDraftRequest(draftId)
-                .then()
-                .statusCode(SC_ACCEPTED);
-
-            loginRestClient.loginBankAdmin();
-            String conversationId = messagesPresentationRestClient.getConversations()
-                .then()
                 .statusCode(SC_OK)
                 .extract()
-                .as(DraftPostResponseBody[].class)[0]
-                .getId();
+                .path("id");
 
-            String conversationDraftId = messagesPresentationRestClient
-                .postConversationDraft(generateConversationDraftsPostRequestBody(), conversationId)
-                .then()
-                .statusCode(SC_ACCEPTED)
-                .extract()
-                .as(MessageDraftsGetResponseBody.class)
-                .getId();
-
-            messagesPresentationRestClient.sendDraftRequest(conversationDraftId)
-                .then()
-                .statusCode(SC_ACCEPTED);
-
-            log.info("Conversation ingested with subject [{}] for user [{}]", draftsPostRequestBody.getSubject(),
-                externalUserId);
+            log.info("Topic ingested with id [{}] for subscriber [{}]", topicId, bankAdmin);
         });
     }
 }
--- src/main/java/com/backbase/ct/bbfuel/data/MessagesDataGenerator.java
+++ src/main/java/com/backbase/ct/bbfuel/data/MessagesDataGenerator.java
@@ -1,37 +1,16 @@
 package com.backbase.ct.bbfuel.data;
 
-import com.backbase.dbs.messages.rest.spec.v4.messagecenter.ConversationMessageDraftPostRequestBody;
-import com.backbase.dbs.messages.rest.spec.v4.messagecenter.MessageDraftsPostRequestBody;
 import com.backbase.dbs.messages.rest.spec.v4.messagecenter.TopicsPostRequestBody;
 import com.github.javafaker.Faker;
 import java.util.Set;
-import org.apache.commons.codec.binary.Base64;
 
 public class MessagesDataGenerator {
 
     private static Faker faker = new Faker();
 
-    public static MessageDraftsPostRequestBody generateDraftsPostRequestBody(String topicId) {
-        return new MessageDraftsPostRequestBody()
-            .withBody(encodeString(faker.lorem().paragraph()))
-            .withSubject(faker.lorem().sentence().replace(".", ""))
-            .withTopic(topicId);
-    }
-
-    public static ConversationMessageDraftPostRequestBody generateConversationDraftsPostRequestBody() {
-        return new ConversationMessageDraftPostRequestBody()
-            .withBody(encodeString(faker.lorem().paragraph()));
-    }
-
     public static TopicsPostRequestBody generateTopicPostRequestBody(Set<String> subscribers) {
         return new TopicsPostRequestBody()
             .withName(faker.lorem().sentence(2, 2))
             .withSubscribers(subscribers);
-    }
-
-    private static String encodeString(String value) {
-        byte[] bodyEncoded = Base64.encodeBase64(value.getBytes());
-
-        return new String(bodyEncoded);
     }
 }

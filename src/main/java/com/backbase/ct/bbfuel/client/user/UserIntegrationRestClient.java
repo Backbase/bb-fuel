9d71229b0c23f4b0d233781960159f6ceebf2d8c
Chris Richards <37043621+reCeLR@users.noreply.github.com>
2020-08-25 12:52:59 +0100

    ID-2678: Integration with identity

--- src/main/java/com/backbase/ct/bbfuel/client/user/UserIntegrationRestClient.java
+++ src/main/java/com/backbase/ct/bbfuel/client/user/UserIntegrationRestClient.java
@@ -1,7 +1,9 @@
 package com.backbase.ct.bbfuel.client.user;
 
 import static com.backbase.ct.bbfuel.util.ResponseUtils.isBadRequestException;
+import static com.backbase.ct.bbfuel.util.ResponseUtils.isConflictException;
 import static org.apache.http.HttpStatus.SC_CREATED;
+import static com.backbase.ct.bbfuel.data.CommonConstants.PROPERTY_IDENTITY_FEATURE_TOGGLE;
 
 import com.backbase.ct.bbfuel.client.common.RestClient;
 import com.backbase.ct.bbfuel.config.BbFuelConfiguration;
@@ -21,6 +23,7 @@ public class UserIntegrationRestClient extends RestClient {
     private final BbFuelConfiguration config;
     private static final String SERVICE_VERSION = "v2";
     private static final String ENDPOINT_USERS = "/users";
+    private static final String ENDPOINT_IDENTITIES = ENDPOINT_USERS + "/identities";
 
     @PostConstruct
     public void init() {
@@ -28,24 +31,46 @@ public class UserIntegrationRestClient extends RestClient {
         setVersion(SERVICE_VERSION);
     }
 
-    public void ingestUserAndLogResponse(UsersPostRequestBody user) {
-        Response response = ingestUser(user);
+    public void ingestAdminAndLogResponse(UsersPostRequestBody user) {
 
-        if (isBadRequestException(response, "User already exists")) {
+        Response response;
+
+        if (this.globalProperties.getBoolean(PROPERTY_IDENTITY_FEATURE_TOGGLE)) {
+            response = importUserIdentity(user);
+        } else {
+            response = ingestUser(user);
+        }
+
+        if (isBadRequestException(response, "User already exists") || isConflictException(response, "User already exists")) {
             log.info("User [{}] already exists, skipped ingesting this user", user.getExternalId());
         } else if (response.statusCode() == SC_CREATED) {
             log.info("User [{}] ingested under legal entity [{}]",
                 user.getExternalId(), user.getLegalEntityExternalId());
         } else {
+            log.info("User [{}] could not be ingested", user.getExternalId());
             response.then()
                 .statusCode(SC_CREATED);
         }
     }
 
-    private Response ingestUser(UsersPostRequestBody body) {
+    public Response ingestUser(UsersPostRequestBody body) {
         return requestSpec()
             .contentType(ContentType.JSON)
             .body(body)
             .post(getPath(ENDPOINT_USERS));
+    }
+
+    public Response importUserIdentity(UsersPostRequestBody body){
+
+        UsersPostRequestBody importBody = new UsersPostRequestBody();
+
+        importBody
+            .withExternalId(body.getExternalId())
+            .withLegalEntityExternalId(body.getLegalEntityExternalId());
+
+        return requestSpec()
+            .contentType(ContentType.JSON)
+            .body(importBody)
+            .post(getPath(ENDPOINT_IDENTITIES));
     }
 }
--- src/main/java/com/backbase/ct/bbfuel/configurator/LegalEntitiesAndUsersConfigurator.java
+++ src/main/java/com/backbase/ct/bbfuel/configurator/LegalEntitiesAndUsersConfigurator.java
@@ -1,18 +1,35 @@
 package com.backbase.ct.bbfuel.configurator;
 
 import static com.backbase.ct.bbfuel.data.CommonConstants.EXTERNAL_ROOT_LEGAL_ENTITY_ID;
+import static com.backbase.ct.bbfuel.data.CommonConstants.PROPERTY_IDENTITY_FEATURE_TOGGLE;
+import static com.backbase.ct.bbfuel.util.ResponseUtils.isBadRequestException;
+import static com.backbase.ct.bbfuel.util.ResponseUtils.isConflictException;
+import static com.backbase.ct.bbfuel.util.ResponseUtils.isNotFoundException;
+import static org.apache.http.HttpStatus.SC_CREATED;
+
 
 import com.backbase.ct.bbfuel.client.accessgroup.UserContextPresentationRestClient;
 import com.backbase.ct.bbfuel.client.common.LoginRestClient;
+import com.backbase.ct.bbfuel.client.legalentity.LegalEntityPresentationRestClient;
 import com.backbase.ct.bbfuel.client.user.UserIntegrationRestClient;
+import com.backbase.ct.bbfuel.client.user.UserPresentationRestClient;
 import com.backbase.ct.bbfuel.data.LegalEntitiesAndUsersDataGenerator;
 import com.backbase.ct.bbfuel.dto.LegalEntityWithUsers;
+
+
 import com.backbase.ct.bbfuel.dto.User;
 import com.backbase.ct.bbfuel.service.LegalEntityService;
+import com.backbase.ct.bbfuel.util.GlobalProperties;
+import com.backbase.dbs.user.integration.rest.spec.v2.users.UsersPostRequestBody;
+
+
 import com.backbase.integration.legalentity.rest.spec.v2.legalentities.LegalEntitiesPostRequestBody;
+import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class LegalEntitiesAndUsersConfigurator {
@@ -20,8 +37,12 @@ public class LegalEntitiesAndUsersConfigurator {
     private final LoginRestClient loginRestClient;
     private final UserContextPresentationRestClient userContextPresentationRestClient;
     private final UserIntegrationRestClient userIntegrationRestClient;
+    private final UserPresentationRestClient userPresentationRestClient;
+    private final LegalEntityPresentationRestClient legalEntityPresentationRestClient;
     private final LegalEntityService legalEntityService;
     private final ServiceAgreementsConfigurator serviceAgreementsConfigurator;
+
+    protected static GlobalProperties globalProperties = GlobalProperties.getInstance();
 
     /**
      * Dispatch the creation of legal entity depending whether given legalEntityWithUsers is a root entity.
@@ -42,7 +63,7 @@ public class LegalEntitiesAndUsersConfigurator {
             .updateMasterServiceAgreementWithExternalIdByLegalEntity(externalLegalEntityId);
 
         User admin = root.getUsers().get(0);
-        this.userIntegrationRestClient.ingestUserAndLogResponse(LegalEntitiesAndUsersDataGenerator
+        this.userIntegrationRestClient.ingestAdminAndLogResponse(LegalEntitiesAndUsersDataGenerator
             .generateUsersPostRequestBody(admin, EXTERNAL_ROOT_LEGAL_ENTITY_ID));
         this.serviceAgreementsConfigurator
             .setEntitlementsAdminUnderMsa(admin.getExternalId(), EXTERNAL_ROOT_LEGAL_ENTITY_ID);
@@ -63,8 +84,45 @@ public class LegalEntitiesAndUsersConfigurator {
 
         legalEntityWithUsers.getUsers().parallelStream()
             .forEach(
-                user -> this.userIntegrationRestClient
-                    .ingestUserAndLogResponse(LegalEntitiesAndUsersDataGenerator
-                        .generateUsersPostRequestBody(user, externalLegalEntityId)));
+                user -> this.ingestUserAndLogResponse(LegalEntitiesAndUsersDataGenerator
+                    .generateUsersPostRequestBody(user, externalLegalEntityId)));
+    }
+    
+
+    private void ingestUserAndLogResponse(UsersPostRequestBody user) {
+
+        com.backbase.dbs.user.presentation.rest.spec.v2.users.UsersPostRequestBody userBody = new com.backbase.dbs.user.presentation.rest.spec.v2.users.UsersPostRequestBody();
+
+        Response response;
+
+        if (this.globalProperties.getBoolean(PROPERTY_IDENTITY_FEATURE_TOGGLE)) {
+            response = this.userIntegrationRestClient.importUserIdentity(user);
+        } else {
+            response = this.userIntegrationRestClient.ingestUser(user);
+        }
+
+        if (isBadRequestException(response, "User already exists") || isConflictException(response, "User already exists")) {
+            log.info("User [{}] already exists, skipped ingesting this user", user.getExternalId());
+        } else if (isNotFoundException(response, "Identity does not exist in Identity Service")) {
+            log.info("Identity for user [{}] not found, creating identity", user.getExternalId());
+
+            String legalEntityId = legalEntityPresentationRestClient
+                .retrieveLegalEntityByExternalId(user.getLegalEntityExternalId()).getId();
+
+            userBody
+                .withExternalId(user.getExternalId())
+                .withFullName(user.getFullName())
+                .withLegalEntityExternalId(user.getLegalEntityExternalId())
+                .withPreferredLanguage(user.getPreferredLanguage());
+
+            this.userPresentationRestClient.createIdentityUserAndLogResponse(userBody, legalEntityId);
+        } else if (response.statusCode() == SC_CREATED) {
+            log.info("User [{}] ingested under legal entity [{}]",
+                user.getExternalId(), user.getLegalEntityExternalId());
+        } else {
+            log.info("User [{}] could not be ingested", user.getExternalId());
+            response.then()
+                .statusCode(SC_CREATED);
+        }
     }
 }

35acff73c619602b5dde86642e7d3cb5d69ba16b
Maithree <maithree@backbase.com>
2021-04-13 14:31:12 +0200

    Added Positive pay to bb-fuel

--- src/main/java/com/backbase/ct/bbfuel/data/CommonConstants.java
+++ src/main/java/com/backbase/ct/bbfuel/data/CommonConstants.java
@@ -81,6 +81,10 @@ public final class CommonConstants {
     public static final String PROPERTY_ACCOUNTSTATEMENTS_MIN = "accountStatement.min";
     public static final String PROPERTY_ACCOUNTSTATEMENTS_MAX = "accountStatement.max";
     public static final String PROPERTY_ACCOUNTSTATEMENTS_USERS = "accountStatement.externalUserIds";
+    public static final String PROPERTY_INGEST_POSITIVE_PAY_CHECKS = "ingest.positivePay";
+    public static final String PROPERTY_POSITIVEPAY_MIN = "positivePay.min";
+    public static final String PROPERTY_POSITIVEPAY_MAX = "positivePay.max";
+    public static final String PROPERTY_POSITIVEPAY_USERS = "positivePay.externalUserIds";
 
     // Product summary
     public static final String PROPERTY_PRODUCTS_JSON_LOCATION = "products.json";

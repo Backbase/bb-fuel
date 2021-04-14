8fb8b43d9aa066dc2802fe8fd598497f7213bd01
Maithree <maithree@backbase.com>
2021-04-14 09:17:49 +0200

    Updated positive pay files

--- src/main/java/com/backbase/ct/bbfuel/data/PositivePayDataGenerator.java
+++ src/main/java/com/backbase/ct/bbfuel/data/PositivePayDataGenerator.java
@@ -3,16 +3,13 @@ package com.backbase.ct.bbfuel.data;
 import com.github.javafaker.Faker;
 import com.backbase.dbs.positivepay.client.api.v1.model.PositivePayPost;
 import com.backbase.dbs.positivepay.client.api.v1.model.Currency;
-import org.apache.commons.lang.time.DateUtils;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 import static com.backbase.ct.bbfuel.util.CommonHelpers.generateRandomAmountInRange;
 import static com.backbase.ct.bbfuel.util.CommonHelpers.generateRandomNumberInRange;
+import static com.backbase.ct.bbfuel.util.CommonHelpers.generateRandomDateInRange;
 
 public class PositivePayDataGenerator {
 
@@ -30,17 +27,8 @@ public class PositivePayDataGenerator {
                 .payeeName(faker.name().firstName() + ' ' + faker.name().lastName())
                 .checkNumber(String.valueOf(generateRandomNumberInRange(234567,1234567890)))
                 .amountDetails(amountDetails)
-                .issueDate(generateRandomDate())
+                .issueDate(generateRandomDateInRange(LocalDate.now().minusDays(40),LocalDate.now().plusDays(10)))
                 .voidCheck(random.nextBoolean());
     }
 
-    public LocalDate generateRandomDate()
-    {
-        LocalDate from = LocalDate.of(2021, 02, 1);
-        LocalDate to = LocalDate.now();
-        long days = from.until(to.plusDays(10), ChronoUnit.DAYS);
-        long randomDays = ThreadLocalRandom.current().nextLong(days + 1);
-        LocalDate randomDate = from.plusDays(randomDays);;
-        return randomDate;
-    }
 }

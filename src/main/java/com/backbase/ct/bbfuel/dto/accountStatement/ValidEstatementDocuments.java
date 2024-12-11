541c6f288e195c014db4a3968bdc5165d2027252
Olek <104128856+aleksanderoBB@users.noreply.github.com>
2024-12-11 13:14:45 +0100

    CONT-3469: adding one more file for account statements (#305)
    * CONT-3469: adding one more file for account statements
    
    * CONT-3469: review foxes
    
    * CONT-3469: review foxes

--- src/main/java/com/backbase/ct/bbfuel/dto/accountStatement/ValidEstatementDocuments.java
+++ src/main/java/com/backbase/ct/bbfuel/dto/accountStatement/ValidEstatementDocuments.java
@@ -5,7 +5,8 @@ public enum ValidEstatementDocuments {
     MSWORD(new EstatetmentDocument("uid_001", DocumentType.MSWORD)),
     JPEG(new EstatetmentDocument("uid_002", DocumentType.JPEG)),
     PDF(new EstatetmentDocument("uid_003", DocumentType.PDF)),
-    TEXT(new EstatetmentDocument("uid_004", DocumentType.TEXT));
+    TEXT(new EstatetmentDocument("uid_004", DocumentType.TEXT)),
+    PDF2(new EstatetmentDocument("YmFja2Jhc2U=", DocumentType.PDF));
 
     private final EstatetmentDocument eStatetmentDocument;
 
